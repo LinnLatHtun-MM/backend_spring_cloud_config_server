90443ed160c54fa95654e21bc379f337cdbfee05
linnlathtun <linnlathtun99.mm@gmail.com>
2025-05-05 14:28:08 +0700

    Enable Config Server

--- src/main/java/per/llt/backend_spring_cloud_config_server/BackendSpringCloudConfigServerApplication.java
+++ src/main/java/per/llt/backend_spring_cloud_config_server/BackendSpringCloudConfigServerApplication.java
@@ -2,8 +2,10 @@ package per.llt.backend_spring_cloud_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // Enable Config Server
 public class BackendSpringCloudConfigServerApplication {
 
     public static void main(String[] args) {
