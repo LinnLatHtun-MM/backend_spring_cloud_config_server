d960d07bd71f125566eab882f7542d19ba45ce81
linnlathtun <linnlathtun99.mm@gmail.com>
2025-05-05 13:30:03 +0700

    Create Project Spring Cloud Config Server

---
+++ src/main/java/per/llt/backend_spring_cloud_config_server/BackendSpringCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package per.llt.backend_spring_cloud_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringCloudConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/per/llt/backend_spring_cloud_config_server/BackendSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package per.llt.backend_spring_cloud_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
